d0266d47c5dfb68643ae2928ed5edd2e46e38542
M. Abuhamad <m.abuhamad@msn.com>
2023-05-01 14:01:17 -0500

    adding the histogram equalizer to the data pipeline

--- train_pix2pix.py
+++ train_pix2pix.py
@@ -3,25 +3,29 @@ from flags import Flags
 import data_loader
 import p2p.modules as modules
 
-flags = Flags().parse()
+def main():
+  train_dataset = data_loader.DataGenerator_PairedReady(flags, flags.data_path).load()
+  test_dataset = data_loader.DataGenerator_PairedReady(flags, flags.test_data_path).load()
+  
+  
+  #Build and train the model
+  model = Pix2Pix(flags)
+  model.compile()
+  history = model.fit(
+    train_dataset,
+    validation_data=test_dataset,
+    epochs=flags.epochs,
+    verbose=1,
+    callbacks=[modules.P2PMonitor(test_dataset, flags)],
+  )
+  
+  
+  model.save_model()
+  model.model_evaluate(test_dataset)
+  model.plot_losses(history.history)
+  
+  
+if __name__ == '__main__':
+  flags = Flags()
+  main()
 
-
-train_dataset = data_loader.DataGenerator_PairedReady(flags, flags.data_path).load()
-test_dataset = data_loader.DataGenerator_PairedReady(flags, flags.test_data_path).load()
-
-
-#Build and train the model
-model = Pix2Pix(flags)
-model.compile()
-history = model.fit(
-  train_dataset,
-  validation_data=test_dataset,
-  epochs=flags.epochs,
-  verbose=1,
-  callbacks=[modules.P2PMonitor(test_dataset, flags)],
-)
-
-
-model.save_model()
-model.model_evaluate(test_dataset)
-model.plot_losses(history.history)
