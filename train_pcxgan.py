9ac71b2ebef92d7b7b0be659b74b5b0f2e5733bb
Rachel Gordon <rgordon@luc.edu>
2023-05-18 11:14:11 -0500

    update pcxgan

--- train_pcxgan.py
+++ train_pcxgan.py
@@ -14,26 +14,26 @@ def main(flags):
     path = f"{data_path}{i}.npz"
     if i == 1:
       data = np.load(path)
-      x_train, y_train = data['arr_0'], data['arr_1']
+      x_train, y_train, mask_train = data['arr_0'], data['arr_1'], data['arr_2']
     else:
       data = np.load(path)
       x_train = np.concatenate((x_train, data['arr_0']), axis=0)
       y_train = np.concatenate((y_train, data['arr_1']), axis=0)
 
   data_test = np.load(test_data_path)
-  x_test, y_test = data_test['arr_0'], data_test['arr_1']
+  x_test, y_test, mask_test = data_test['arr_0'], data_test['arr_1'], data_test['arr_2']
 
   
   #Build and train the model
   model = PCxGAN(flags)
   model.compile()
   history = model.fit(
-    x_train, y_train,
-    validation_data=(x_test, y_test),
+    x_train, y_train, mask_train,
+    validation_data=(x_test, y_test, mask_test),
     epochs=flags.epochs,
     verbose=1,
     batch_size = flags.batch_size,
-    callbacks=[modules.GanMonitor((x_test[5:8], y_test[5:8]), flags)],
+    callbacks=[modules.GanMonitor((x_test[5:8], y_test[5:8], mask_test[5:8]), flags)],
   )
   
   
