276656bfd7484158712a61bf9c98c446f8c95fee
Rachel Gordon <rgordon@luc.edu>
2023-05-22 14:59:01 -0500

    fix upsample

--- pcxgan/modules.py
+++ pcxgan/modules.py
@@ -114,7 +114,7 @@ class DownsampleModule(kr.layers.Layer):
 	def call(self, inputs__):
 		return self.block(inputs__)
 
-
+'''
 class UpsampleModule(kr.layers.Layer):
     def __init__(self, channels, filter_size, batch_norm=True, dropout=True, apply_activation=True, **kwargs):
         super(UpsampleModule, self).__init__(**kwargs)
@@ -159,51 +159,9 @@ class UpsampleModule(kr.layers.Layer):
         if self.apply_activation:
             x = self.leaky_relu(x)
         return x
-    
+'''   
 
-'''
-class UpsampleModule(kr.layers.Layer):
-	def __init__(self, channels, filter_size, batch_norm=True, dropout=True,
-							 apply_activation=True, **kwargs):
-		super().__init__(**kwargs)
-		gamma_init = kr.initializers.RandomNormal(mean=0.0, stddev=0.02)
-		self.block = kr.Sequential()
-		self.strides = 2
-		self.apply_activation = apply_activation
-		
-		# self.block.add(kr.layers.UpSampling2D((2, 2)))
-		
-		self.block.add(kr.layers.Conv2DTranspose(channels, filter_size, strides=self.strides, padding='same',
-																						 kernel_initializer=kr.initializers.RandomNormal(stddev=0.02),
-																						 kernel_regularizer=kr.regularizers.l1_l2(l1=1e-5, l2=1e-5),
-																						 activity_regularizer=kr.regularizers.l2(1e-5)))
-		
-		if batch_norm:
-			#self.block.add(kr.layers.BatchNormalization())
-			#prev_layer = self.block.layers[0]
-			#prev_layer_input = prev_layer.input
-			#this_layer_input = prev_layer_input.copy()
-			#this_layer_input = tf.cast(this_layer_input, tf.int32)
-			
-			self.block.add(kr.layers.GroupNormalization(groups=channels, gamma_initializer=gamma_init))
 
-			#layer = self.block.layers[1]
-			#layer.input = this_layer_input
-			
-		if dropout:
-			self.block.add(kr.layers.Dropout(0.5))
-		if self.apply_activation:
-			self.block.add(kr.layers.LeakyReLU(0.2))
-	
-		# Iterate over layers and print their names and indices
-		for i, layer in enumerate(self.block.layers):
-			print("Layer Name:", layer.name)
-			print("Layer Index:", i)
-			print()
-
-	def call(self, inputs_):
-		return self.block(inputs_)
-	
 
 
 class UpsampleModule(kr.layers.Layer):
@@ -224,67 +182,17 @@ class UpsampleModule(kr.layers.Layer):
 		
 		if batch_norm:
 			#self.block.add(kr.layers.BatchNormalization())
-			#prev_layer = self.block.layers[0]
-			#prev_layer_input = prev_layer.input
-			#this_layer_input = prev_layer_input.copy()
-			#this_layer_input = tf.cast(this_layer_input, tf.int32)
-			
-			self.block.add(kr.layers.GroupNormalization(groups=channels, gamma_initializer=gamma_init))
-
-			#layer = self.block.layers[1]
-			#layer.input = this_layer_input
-			
+			self.block.add(kr.layers.GroupNormalization(groups=int(channels), gamma_initializer=gamma_init))
 		if dropout:
 			self.block.add(kr.layers.Dropout(0.5))
 		if self.apply_activation:
 			self.block.add(kr.layers.LeakyReLU(0.2))
-	
-		# Iterate over layers and print their names and indices
-		for i, layer in enumerate(self.block.layers):
-			print("Layer Name:", layer.name)
-			print("Layer Index:", i)
-			print()
 
 	def call(self, inputs_):
 		return self.block(inputs_)
 
 
-class UpsampleModule(kr.layers.Layer):
-    def __init__(self, channels, filter_size, batch_norm=True, dropout=True,
-                 apply_activation=True, **kwargs):
-        super().__init__(**kwargs)
-        gamma_init = kr.initializers.RandomNormal(mean=0.0, stddev=0.02)
-        self.float_branch = kr.Sequential()
-        self.int_branch = kr.Sequential()
-        self.strides = 2
-        self.apply_activation = apply_activation
 
-        self.float_branch.add(kr.layers.Conv2DTranspose(channels, filter_size, strides=self.strides, padding='same',
-                                                 kernel_initializer=kr.initializers.RandomNormal(stddev=0.02),
-                                                 kernel_regularizer=kr.regularizers.l1_l2(l1=1e-5, l2=1e-5),
-                                                 activity_regularizer=kr.regularizers.l2(1e-5)))
-        if batch_norm:
-            self.int_branch.add(kr.layers.GroupNormalization(groups=channels, gamma_initializer=gamma_init))
-        if dropout:
-            self.int_branch.add(kr.layers.Dropout(0.5))
-        if self.apply_activation:
-            self.int_branch.add(kr.layers.LeakyReLU(0.2))
-
-
-    def call(self, inputs_):
-	
-        # Apply the float branch to the float inputs
-        float_outputs = self.float_branch(inputs_)
-
-        # Process the int part of the inputs (assuming it's in the second tensor)
-        int_inputs = tf.cast(float_outputs, tf.int32)
-        int_outputs = self.process_int_branch(int_inputs)
-
-        return int_outputs
-
-    def process_int_branch(self, int_inputs):
-        return self.int_branch(int_inputs)
-'''
 
 
 class Encoder(kr.Model):
