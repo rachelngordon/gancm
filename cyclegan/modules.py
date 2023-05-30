861fd320a8ce9f40bcc356b161da8a39f3273a1c
Rachel Gordon <rgordon@luc.edu>
2023-05-30 09:38:36 -0500

    change decoder

--- cyclegan/modules.py
+++ cyclegan/modules.py
@@ -149,10 +149,17 @@ class DecoderModule(kr.Model):
         #define encoder
         self.decoder = kr.models.Sequential()
         self.decoder._name = "Decoder"
+        self.decoder.add(ResnetBlockT(channels, 1))
+        self.decoder.add(ResnetBlockT(channels//2, 1))
+        self.decoder.add(ResnetBlockT(channels//2, 1))
+        self.decoder.add(ResnetBlockT(channels//4, 1))
+
+        '''
         self.decoder.add(ResnetBlockT(channels//4, 1))
         self.decoder.add(ResnetBlockT(channels//2, 1))
         self.decoder.add(ResnetBlockT(channels//2, 1))
         self.decoder.add(ResnetBlockT(channels, 1))
+        '''
 
 
     def call(self, inputs__):
