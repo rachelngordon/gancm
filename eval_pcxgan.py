be5f77eb00bce3fce25611d45f462667586400fc
Rachel Gordon <rgordon@luc.edu>
2023-06-02 10:33:42 -0500

    eval pcxgan

---
+++ eval_pcxgan.py
@@ -0,0 +1,30 @@
+from flags import Flags
+import numpy as np
+import p2p.modules as modules
+import tensorflow.keras as kr
+import evaluate
+
+def main(flags):
+
+
+  test_data_path = f"/grand/EVITA/ct-mri/data/CV/norm_mask_neg1pos1_fold{flags.test_fold}.npz"
+	
+
+  data_test = np.load(test_data_path)
+  x_test, y_test = data_test['arr_0'], data_test['arr_1']
+
+
+
+  # load and evaluate the model
+
+  # get model path
+  path = "/grand/EVITA/ct-mri/pcxgan/models/" + flags.name
+
+  model = kr.models.load_model(path)
+  evaluate.model_evaluate(flags, model, (x_test, y_test))
+  
+  
+if __name__ == '__main__':
+  flags = Flags().parse()
+  main(flags)
+
--- multiprocess2.py
+++ multiprocess2.py
@@ -3,8 +3,10 @@ import multiprocessing
 
 # Define the command lines and GPU IDs
 command_lines = [
-    ("python3 train_pcxgan_ct.py --exp_name pcx_fold2345_1000 --test_fold 1 --epochs 1000 --batch_size 32", 0),
-    ("python3 train_pix2pix.py --exp_name p2p_fold2345_1000 --test_fold 1 --epochs 1000", 1)
+    ("python3 eval_pcxgan.py --exp_name pcx_fold1234_1000 --test_fold 5 --batch_size 32", 0),
+    ("python3 eval_pcxgan.py --exp_name pcx_fold1235_1000 --test_fold 4 --batch_size 32", 1),
+    ("python3 eval_pcxgan.py --exp_name pcx_fold1245_1000 --test_fold 3 --batch_size 32", 2),
+    ("python3 eval_pcxgan.py --exp_name pcx_fold1345_1000 --test_fold 2 --batch_size 32", 3)
 ]
 
 # Function to run a command on a GPU
