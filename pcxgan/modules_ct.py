5817475439236cbc9882388aa2f9c740b20451f2
Rachel Gordon <rgordon@luc.edu>
2023-06-09 11:25:15 -0500

    fix error

--- pcxgan/modules_ct.py
+++ pcxgan/modules_ct.py
@@ -10,7 +10,7 @@ import os
 from datetime import datetime
 import data_loader
 import flags
-import tensorflow_addons as tfa
+#import tensorflow_addons as tfa
 
 
 class SPADE(kr.layers.Layer):
@@ -106,7 +106,7 @@ class DownsampleModule(kr.layers.Layer):
 
 		
 		if apply_norm:
-			self.block.add(tfa.layers.GroupNormalization(groups=channels))
+			self.block.add(kr.layers.GroupNormalization(groups=channels))
 		if batch_norm:
 			self.block.add(kr.layers.BatchNormalization())
 		if self.apply_activation:
