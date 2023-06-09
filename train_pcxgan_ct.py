e46d4c68ffb24d31f92b923795de5ded1f95cf5a
Rachel Gordon <rgordon@luc.edu>
2023-06-09 12:15:44 -0500

    equalized flag

--- train_pcxgan_ct.py
+++ train_pcxgan_ct.py
@@ -10,9 +10,13 @@ import tensorflow.keras as kr
 
 
 def main(flags):
-  
-  data_path = "/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold"
-  test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold{flags.test_fold}"
+
+  if flags.equalized:
+    data_path = "/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold"
+    test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold{flags.test_fold}"
+  else:
+    data_path = "/media/aisec-102/DATA3/rachel/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold"
+    test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold2{flags.test_fold}"
 
   # load data on polaris
   #data_path = "/grand/EVITA/ct-mri/data/mask_data/norm_mask_neg1pos1_fold"
