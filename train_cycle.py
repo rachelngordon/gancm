ac850be1bac542066988ffbaba8be842dbfcfffa
Rachel Gordon <rgordon@luc.edu>
2023-06-09 15:54:46 -0500

    train cycle

--- train_cycle.py
+++ train_cycle.py
@@ -10,42 +10,34 @@ import numpy as np
 
 def main(flags):
 
-  data_path = "/media/aisec-102/DATA3/rachel/data/CV/normalized_neg1pos1_fold"
-  test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/normalized_neg1pos1_fold{flags.test_fold}.npz"
-	
-  folds = list(range(1,6))
-  folds.remove(flags.test_fold)
+  if flags.equalized:
+    data_path = "/media/aisec-102/DATA3/rachel/data/CV/eq_paired/normalized_neg1pos1_fold"
+    test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/eq_paired/normalized_neg1pos1_fold{flags.test_fold}"
+  else:
+    data_path = "/media/aisec-102/DATA3/rachel/data/CV/no_eq_paired/no_eq_neg1pos1_fold"
+    test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/no_eq_paired/no_eq_neg1pos1_fold{flags.test_fold}"
 
-  for i in folds:
-    path = f"{data_path}{i}.npz"
-    
-    if i == folds[0]:
-      data = np.load(path)
-      x_train, y_train = data['arr_0'], data['arr_1']
-    else:
-      data = np.load(path)
-      x_train = np.concatenate((x_train, data['arr_0']), axis=0)
-      y_train = np.concatenate((y_train, data['arr_1']), axis=0)
 
-  data_test = np.load(test_data_path)
-  x_test, y_test = data_test['arr_0'], data_test['arr_1']
+
+  train_data = data_loader.DataGenerator_PairedReady(flags, data_path, if_train=True).load()
+  test_data = data_loader.DataGenerator_PairedReady(flags, test_data_path, if_train=False).load()
 
   
   #Build and train the model
   model = CycleGAN(flags)
   model.compile()
   history = model.fit(
-    x_train, y_train,
-    validation_data=(x_test, y_test),
+    train_data,
+    validation_data=test_data,
     epochs=flags.epochs,
     verbose=1,
     batch_size = flags.batch_size,
-    callbacks=[modules.CycleMonitor((x_test[5:8], y_test[5:8]), flags)],
+    callbacks=[modules.CycleMonitor(test_data, flags)],
   )
   
   
   model.save_model(flags)
-  model.model_evaluate((x_test, y_test))
+  model.model_evaluate(test_data)
   model.plot_losses(history.history)
   
   
