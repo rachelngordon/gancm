fed52db2bd8d24abaf4cc1dac6281a3beff7a40f
Rachel Gordon <rgordon@luc.edu>
2023-06-09 20:17:03 -0500

    prep for polaris

--- train_pcxgan_ct.py
+++ train_pcxgan_ct.py
@@ -12,18 +12,18 @@ import tensorflow.keras as kr
 def main(flags):
 
   if flags.equalized == True:
-    data_path = "/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold"
-    test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold{flags.test_fold}"
+    #data_path = "/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold"
+    #test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/eq_mask/norm_mask_neg1pos1_fold{flags.test_fold}"
 
-    #data_path = "/grand/EVITA/ct-mri/data/CV/eq_mask/norm_mask_neg1pos1_fold"
-    #test_data_path = f"/grand/EVITA/ct-mri/data/CV/eq_mask/norm_mask_neg1pos1_fold{flags.test_fold}"
+    data_path = "/grand/EVITA/ct-mri/data/CV/eq_mask/norm_mask_neg1pos1_fold"
+    test_data_path = f"/grand/EVITA/ct-mri/data/CV/eq_mask/norm_mask_neg1pos1_fold{flags.test_fold}"
   else:
 
-    data_path = "/media/aisec-102/DATA3/rachel/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold"
-    test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold{flags.test_fold}"
+    #data_path = "/media/aisec-102/DATA3/rachel/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold"
+    #test_data_path = f"/media/aisec-102/DATA3/rachel/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold{flags.test_fold}"
 
-    #data_path = "/grand/EVITA/ct-mri/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold"
-    #test_data_path = f"/grand/EVITA/ct-mri/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold{flags.test_fold}"
+    data_path = "/grand/EVITA/ct-mri/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold"
+    test_data_path = f"/grand/EVITA/ct-mri/data/CV/no_eq_mask/no_eq_mask_neg1pos1_fold{flags.test_fold}"
 
 
 
