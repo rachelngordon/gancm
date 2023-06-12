1e6d3b367da72be4c5f66bfccdd61ca247b43efc
Rachel Gordon <rgordon@luc.edu>
2023-06-12 17:05:30 -0500

    fix error

--- cyclegan/cyclegan.py
+++ cyclegan/cyclegan.py
@@ -303,12 +303,12 @@ class CycleGAN(kr.Model):
 
         results = []
 
-        num_batches = len(test_data[0]//self.batch_size)
+        #num_batches = len(test_data[0]//self.batch_size)
 
-        for i in range(0, num_batches, self.batch_size):
-            ct, mri = test_data[0][i:i+self.batch_size], test_data[1][i:i+self.batch_size]
+        #for i in range(0, num_batches, self.batch_size):
+            #ct, mri = test_data[0][i:i+self.batch_size], test_data[1][i:i+self.batch_size]
 
-        #for ct, mri in test_data:
+        for ct, mri in test_data:
             
             fake_mri = self.generator_mri(ct)
 
