3432bcf7f72c8bfa59bf50ae469314ea1f9769c5
Rachel Gordon <rgordon@luc.edu>
2023-06-12 14:09:11 -0500

    load data

--- data_loader.py
+++ data_loader.py
@@ -51,7 +51,7 @@ class DataGenerator_Ready(kr.utils.Sequence):
 
 	# load test fold
     else: 
-        path = f"{data_path}.npz"
+        path = f"{data_path}{flags.test_fold}.npz"
         data = np.load(path)
         x, y, z = data['arr_0'], data['arr_1'], data['arr_2']
         return x, y, z
