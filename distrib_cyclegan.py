4f82b9b220d7fb298dff1e181a10e83842125089
Rachel Gordon <rgordon@luc.edu>
2023-06-20 15:35:16 -0500

    fix errors

--- distrib_cyclegan.py
+++ distrib_cyclegan.py
@@ -5,6 +5,7 @@ import data_loader
 import cyclegan.modules as modules
 import os
 import numpy as np
+import tensorflow as tf
 
 def main(flags):
 
@@ -12,18 +13,21 @@ def main(flags):
   train_data = data_loader.DataGenerator_PairedReady(flags, flags.data_path, if_train=True).load()
   test_data = data_loader.DataGenerator_PairedReady(flags, flags.data_path, if_train=False).load()
 
-  
-  #Build and train the model
-  model = CycleGAN(flags)
-  model.compile()
-  history = model.fit(
-    train_data,
-    validation_data=test_data,
-    epochs=flags.epochs,
-    verbose=1,
-    batch_size = flags.batch_size,
-    callbacks=[modules.CycleMonitor(test_data, flags)],
-  )
+  strategy = tf.distribute.MirroredStrategy()
+
+  with strategy.scope():
+
+    #Build and train the model
+    model = CycleGAN(flags)
+    model.compile()
+    history = model.fit(
+        train_data,
+        validation_data=test_data,
+        epochs=flags.epochs,
+        verbose=1,
+        batch_size = flags.batch_size,
+        callbacks=[modules.CycleMonitor(test_data, flags)],
+    )
   
   
   model.save_model(flags)
