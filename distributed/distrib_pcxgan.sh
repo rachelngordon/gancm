62e434a9f61ca3bb62cf91c45f957483179b6e2d
Rachel Gordon <rgordon@luc.edu>
2023-06-21 11:31:16 -0500

    move file

--- distributed/distrib_pcxgan.sh
+++ distributed/distrib_pcxgan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 module load conda
 conda activate
-python3 /grand/EVITA/ct-mri/pcxgan/distributed/distrib_pcxgan.py --exp_name distrib_pcxgan_test --test_fold 5 --epochs 1000 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/no_eq_mask/norm_mask_neg1pos1_fold'
+python3 /grand/EVITA/ct-mri/pcxgan/distrib_pcxgan.py --exp_name distrib_pcxgan_test --test_fold 5 --epochs 1000 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/no_eq_mask/norm_mask_neg1pos1_fold'
