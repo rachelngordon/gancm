6c9a3dd99e25749cf4cf692be28e1d2086eb1096
Rachel Gordon <rgordon@luc.edu>
2023-06-27 13:12:18 -0500

    change batch size

--- p2p_distributed.sh
+++ p2p_distributed.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 module load conda
 conda activate 
-python3 ~/pcxgan/p2p_distributed.py --exp_name p2p_distributed_test --test_fold 1 --epochs 10 --epoch_interval 2 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'
+python3 ~/pcxgan/p2p_distributed.py --exp_name p2p_distributed_test --test_fold 1 --epochs 10 --epoch_interval 2 --batch_size 2 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'
