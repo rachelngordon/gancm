c177cba4dc2770c452dc07bc3f29e8472f684731
Rachel Gordon <rgordon@luc.edu>
2023-06-28 11:44:42 -0500

    test p2p

--- pix2pix_distrib.py
+++ pix2pix_distrib.py
@@ -2,7 +2,7 @@
 import tensorflow.keras as kr
 import tensorflow as tf
 import numpy as np
-from .distributed import modules
+import modules_distrib as modules
 import loss
 import evaluate
 import os
