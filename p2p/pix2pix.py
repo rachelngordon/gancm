d1e15481de3164581656eae401216126d292b80c
Rachel Gordon <rgordon@luc.edu>
2023-06-28 11:21:25 -0500

    test p2p

--- p2p/pix2pix.py
+++ p2p/pix2pix.py
@@ -262,6 +262,8 @@ class Pix2Pix(kr.Model):
 
 	def DiscriminatorLoss(self, is_real, y_pred):
 		label = 1.0 if is_real else -1.0
-		return kr.losses.Hinge(label, y_pred, reduction='sum_over_batch_size')
+		h = kr.losses.Hinge(reduction='sum_over_batch_size')
+
+		return h(label, y_pred)
 
 
