3ba23c9e3231fb2e9315281ebf50bef1b4f29b76
Rachel Gordon <rgordon@luc.edu>
2023-07-05 11:39:33 -0500

    run on server

--- train_p2p_distrib.py
+++ train_p2p_distrib.py
@@ -18,19 +18,20 @@ def main(flags):
   # define the distribution strategy
   strategy = tf.distribute.MirroredStrategy()
 
+  # define VGG model for VGG loss
+  encoder_layers = [
+          "block1_conv1",
+          "block2_conv1",
+          "block3_conv1",
+          "block4_conv1",
+          "block5_conv1",
+      ]
+  weights = [1.0 / 32, 1.0 / 16, 1.0 / 8, 1.0 / 4, 1.0]
+  vgg = kr.applications.VGG19(include_top=False, weights="imagenet")
+  layer_outputs = [vgg.get_layer(x).output for x in encoder_layers]
+  vgg_model = kr.Model(vgg.input, layer_outputs, name="VGG")
+  
   with strategy.scope() as s:
-    # define VGG model for VGG loss
-    encoder_layers = [
-						"block1_conv1",
-						"block2_conv1",
-						"block3_conv1",
-						"block4_conv1",
-						"block5_conv1",
-				]
-    weights = [1.0 / 32, 1.0 / 16, 1.0 / 8, 1.0 / 4, 1.0]
-    vgg = kr.applications.VGG19(include_top=False, weights="imagenet")
-    layer_outputs = [vgg.get_layer(x).output for x in encoder_layers]
-    vgg_model = kr.Model(vgg.input, layer_outputs, name="VGG")
 
     #Build the model
     model = Pix2Pix(flags, vgg_model, weights, strategy.num_replicas_in_sync)
