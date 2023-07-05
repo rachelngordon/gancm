cb34431c2dd69e39a4a22fe3f8ce0cbd10b4f5d3
Rachel Gordon <rgordon@luc.edu>
2023-07-05 11:33:50 -0500

    run on server

--- train_p2p_distrib.py
+++ train_p2p_distrib.py
@@ -37,35 +37,35 @@ def main(flags):
     model.compile()
 
 
-  history = model.fit(
-    train_data,
-    validation_data=test_data,
-    epochs=flags.epochs,
-    verbose=1,
-    batch_size = flags.batch_size,
-    callbacks=[modules.P2PMonitor(test_data, flags)],
-  )
+    history = model.fit(
+      train_data,
+      validation_data=test_data,
+      epochs=flags.epochs,
+      verbose=1,
+      batch_size = flags.batch_size,
+      callbacks=[modules.P2PMonitor(test_data, flags)],
+    )
 
-  end_time = time.time()
+    end_time = time.time()
 
-  # Calculate the training duration
-  training_duration = end_time - start_time
+    # Calculate the training duration
+    training_duration = end_time - start_time
 
-  # Print the training time
-  print("Training time: {:.2f} seconds".format(training_duration))
+    # Print the training time
+    print("Training time: {:.2f} seconds".format(training_duration))
 
-  '''
-  # Save the training time to a file
-  filename = '/grand/EVITA/ct-mri/exp_results/time_train/' + flags.exp_name + "_train_time.txt"
-  with open(filename, "w") as file:
-      file.write("Training time: {:.2f} seconds".format(training_duration))
-      print("Training time saved to", filename)
-  '''
-  
-  
-  model.save_model(flags)
-  model.model_evaluate(test_data)
-  model.plot_losses(history.history)
+    '''
+    # Save the training time to a file
+    filename = '/grand/EVITA/ct-mri/exp_results/time_train/' + flags.exp_name + "_train_time.txt"
+    with open(filename, "w") as file:
+        file.write("Training time: {:.2f} seconds".format(training_duration))
+        print("Training time saved to", filename)
+    '''
+    
+    
+    model.save_model(flags)
+    model.model_evaluate(test_data)
+    model.plot_losses(history.history)
   
   
 if __name__ == '__main__':
