2ef73fa3279621bbc6d3e57ae47877bdf54d77fa
Rachel Gordon <rgordon@luc.edu>
2023-07-12 13:43:06 -0500

    fix error

--- pcx_just_seg_extra.py
+++ pcx_just_seg_extra.py
@@ -5,7 +5,8 @@ import multiprocessing
 command_lines = [
     ("python3 ~/pcxgan/train_pcxgan_just_mask.py --exp_name pcx_just_seg_eq_2345 --test_fold 1 --epochs 500 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/eq_mask/norm_mask_neg1pos1_fold'", 0),
     ("python3 ~/pcxgan/train_pcxgan_just_mask.py --exp_name pcx_just_seg_no_eq_2345 --test_fold 1 --epochs 500 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/no_eq_mask/norm_mask_neg1pos1_fold'", 1), 
-    ("python3 ~/pcxgan/train_pcxgan_just_mask.py --exp_name pcx_just_seg_avg_eq_2345 --test_fold 1 --epochs 500 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold'", 2)
+    ("python3 ~/pcxgan/train_pcxgan_just_mask.py --exp_name pcx_just_seg_avg_eq_2345 --test_fold 1 --epochs 500 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold'", 2),
+    ("python3 ~/pcxgan/train_pcxgan_no_mask.py --exp_name pcx_no_mask_aug_2345 --test_fold 1 --epochs 500 --batch_size 1 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 3)
 ]
 
 # Function to run a command on a GPU
