e57422b25c8e79c23525eb7551ad6acd186f85af
Rachel Gordon <rgordon@luc.edu>
2023-07-13 15:40:12 -0500

    run exp

--- unet/modules.py
+++ unet/modules.py
@@ -152,9 +152,9 @@ def generator_loss(gen_output, target):
 
 
 def discriminator_loss(disc_real_output, disc_generated_output):
-  real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
+  real_loss = loss_object(disc_real_output, disc_real_output)
 
-  generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
+  generated_loss = loss_object(disc_generated_output, disc_generated_output)
 
   total_disc_loss = real_loss + generated_loss
 
