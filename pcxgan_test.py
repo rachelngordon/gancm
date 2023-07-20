2ef5ac23fbe067cb22089714b0cece76d9d39d23
Rachel Gordon <rgordon@luc.edu>
2023-07-20 09:24:08 -0500

    test two gpus

--- pcxgan_test.py
+++ pcxgan_test.py
@@ -3,8 +3,8 @@ import multiprocessing
 
 # Define the command lines and GPU IDs
 command_lines = [
-    ("python3 ~/pcxgan/pcxgan_train_test.py --exp_name pcx_test_1234 --test_fold 5 --epochs 500 --batch_size 8 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 0, 1),
-    ("python3 ~/pcxgan/pcxgan_train_test.py --exp_name pcx_test_1235 --test_fold 4 --epochs 500 --batch_size 8 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 2, 3)
+    ("python3 ~/pcxgan/pcxgan_train_test.py --exp_name pcx_test_1234 --test_fold 5 --epochs 10 --epoch_interval 1 --batch_size 8 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 0, 1),
+    ("python3 ~/pcxgan/pcxgan_train_test.py --exp_name pcx_test_1235 --test_fold 4 --epochs 10 --epoch_interval 1 --batch_size 8 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 2, 3)
 ]
 
 # Function to run a command on two GPUs
