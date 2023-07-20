8077f7401536d1130ef08277d13968ce8d84ad5f
Rachel Gordon <rgordon@luc.edu>
2023-07-20 10:00:58 -0500

    exp

--- aug_seg32_1.py
+++ aug_seg32_1.py
@@ -3,8 +3,8 @@ import multiprocessing
 
 # Define the command lines and GPU IDs
 command_lines = [
-    ("python3 ~/pcxgan/train_pcxgan_aug_mask.py --exp_name pcx_seg_ct_aug_32_1234 --test_fold 5 --epochs 500 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_mask/norm_neg1pos1_fold'", 0, 1),
-    ("python3 ~/pcxgan/train_pcxgan_aug_mask.py --exp_name pcx_seg_ct_aug_32_1235 --test_fold 4 --epochs 500 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_mask/norm_neg1pos1_fold'", 2, 3)
+    ("python3 ~/pcxgan/train_pcxgan_aug_mask.py --exp_name pcx_seg_ct_aug_32_1234 --test_fold 5 --epochs 500 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold'", 0, 1),
+    ("python3 ~/pcxgan/train_pcxgan_aug_mask.py --exp_name pcx_seg_ct_aug_32_1235 --test_fold 4 --epochs 500 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold'", 2, 3)
 ]
 
 # Function to run a command on two GPUs
