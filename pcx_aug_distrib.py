8bc5cf68f9ba984676b02ae593704dfc01f0b058
Rachel Gordon <rgordon@luc.edu>
2023-07-25 13:56:19 -0500

    fix error

--- pcx_aug_distrib.py
+++ pcx_aug_distrib.py
@@ -2,7 +2,7 @@
 import tensorflow.keras as kr
 import tensorflow as tf
 import numpy as np
-from . import modules_mask as modules
+import pcxgan.modules_mask as modules
 import loss
 import evaluate
 from datetime import datetime
