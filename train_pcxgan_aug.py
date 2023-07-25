7d19d658fe18ad8933fd8c25e77466e169b1b633
Rachel Gordon <rgordon@luc.edu>
2023-07-25 08:44:51 -0500

    fix

--- train_pcxgan_aug.py
+++ train_pcxgan_aug.py
@@ -11,41 +11,41 @@ import time
 
 def main(flags):
     # Get the available GPUs
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    #gpus = tf.config.experimental.list_physical_devices('GPU')
 
     # Check if at least two GPUs are available
-    if len(gpus) < 2:
-        raise RuntimeError("At least two GPUs are required for this script.")
+    #if len(gpus) < 2:
+        #raise RuntimeError("At least two GPUs are required for this script.")
 
     # Get the name of the first GPU without "physical_device:"
-    gpu_name_1 = gpus[0].name.replace("physical_device:", "")
+    #gpu_name_1 = gpus[0].name.replace("physical_device:", "")
 
     # Load augmented training data on the first GPU
-    with tf.device(gpu_name_1):
-        train_data = data_loader.DataGeneratorAug(flags, flags.data_path, if_train=True).load()
+    #with tf.device(gpu_name_1):
+    train_data = data_loader.DataGeneratorAug(flags, flags.data_path, if_train=True).load()
 
-        # Load test data without augmentation
-        test_data = data_loader.DataGenerator_PairedReady(flags, flags.data_path, if_train=False).load()
+    # Load test data without augmentation
+    test_data = data_loader.DataGenerator_PairedReady(flags, flags.data_path, if_train=False).load()
 
     # Start the timer
     start_time = time.time()
 
     # Get the name of the second GPU without "physical_device:"
-    gpu_name_2 = gpus[1].name.replace("physical_device:", "")
+    #gpu_name_2 = gpus[1].name.replace("physical_device:", "")
 
     # Build and train the model on the second GPU
-    with tf.device(gpu_name_2):
-        model = PCxGAN(flags)
-        model.compile()
+    #with tf.device(gpu_name_2):
+    model = PCxGAN(flags)
+    model.compile()
 
-        history = model.fit(
-            train_data,
-            validation_data=test_data,
-            epochs=flags.epochs,
-            verbose=1,
-            batch_size=flags.batch_size,
-            callbacks=[modules.GanMonitor(test_data, flags)],
-        )
+    history = model.fit(
+        train_data,
+        validation_data=test_data,
+        epochs=flags.epochs,
+        verbose=1,
+        batch_size=flags.batch_size,
+        callbacks=[modules.GanMonitor(test_data, flags)],
+    )
 
     end_time = time.time()
 
