5c08496ec9af4f0b760fa1bc95cd97098ab616ad
Rachel Gordon <rgordon@luc.edu>
2023-07-26 08:37:08 -0500

    test distrib

---
+++ test_pcx_distrib.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+module load conda
+conda activate 
+python3 ~/pcxgan/pcx_distrib_test.py --exp_name pcx_distributed_test --test_fold 1 --epochs 10 --epoch_interval 1 --batch_size 64 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'
