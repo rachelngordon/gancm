552d15c76fbbc1a013f0772f2c3389eee3a752c4
Rachel Gordon <rgordon@luc.edu>
2023-08-17 14:26:37 -0500

    update experiments

--- aug_32_5000_2.py
+++ aug_32_5000_2.py
@@ -3,8 +3,8 @@ import multiprocessing
 
 # Define the command lines and GPU IDs
 command_lines = [
-    ("python3 ~/pcxgan/train_pcxgan_aug.py --exp_name pcx_aug_32_1245 --test_fold 3 --epochs 5000 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 0, 1)
-    ("python3 ~/pcxgan/train_pcxgan_aug.py --exp_name pcx_aug_32_1345 --test_fold 2 --epochs 5000 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_mask_neg1pos1_fold'", 2, 3)
+    ("python3 ~/pcxgan/train_pcxgan_aug.py --exp_name pcx_no_mask_aug_32_1245 --test_fold 3 --epochs 5000 --batch_size 32 --epoch_interval 250 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 0, 1),
+    ("python3 ~/pcxgan/train_pcxgan_aug.py --exp_name pcx_no_mask_aug_32_1345 --test_fold 2 --epochs 5000 --batch_size 32 --epoch_interval 250 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 2, 3)
 ]
 
 # Function to run a command on two GPUs
--- aug_32_5000_3.sh
+++ aug_32_5000_3.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 module load conda
 conda activate 
-python3 ~/pcxgan/aug_32_epochs.py
+python3 ~/pcxgan/aug_32_5000_3.py
