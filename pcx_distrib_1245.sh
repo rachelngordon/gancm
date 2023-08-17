b32fe0c913483360bd9d14f8f069319a96a540a7
Rachel Gordon <rgordon@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2023-08-17 19:44:15 +0000

    distrib

---
+++ pcx_distrib_1245.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+module load conda
+conda activate 
+python3 ~/pcxgan/train_pcx_aug_distrib.py --exp_name pcx_aug_32_1245_5000_dist --test_fold 3 --epochs 5000 --epoch_interval 250 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'
