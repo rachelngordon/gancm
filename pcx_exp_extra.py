5b5ece269eb6fe305ea1c4bd59a8b7a2404f6d95
Rachel Gordon <rgordon@luc.edu>
2023-09-19 15:27:59 -0500

    fix error

--- pcx_exp_extra.py
+++ pcx_exp_extra.py
@@ -3,8 +3,8 @@ import multiprocessing
 
 # Define the command lines and GPU IDs
 command_lines = [
-    ("python3 ~/pcxgan/train_pcxgan_aug_no_mask.py --exp_name pcx_no_mask_aug_48_2345 --test_fold 1 --epochs 500 --batch_size 48 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 0, 1),
-    ("python3 ~/pcxgan/train_pcxgan_aug_no_mask.py --exp_name pcx_no_mask_aug_32_2345 --test_fold 1 --epochs 500 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 2, 3)
+    ("python3 ~/pcxgan/train_pcxgan_aug.py --exp_name pcx_no_mask_aug_48_2345 --test_fold 1 --epochs 500 --batch_size 48 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 0, 1),
+    ("python3 ~/pcxgan/train_pcxgan_aug.py --exp_name pcx_no_mask_aug_32_2345 --test_fold 1 --epochs 500 --batch_size 32 --data_path '/grand/EVITA/ct-mri/data/CV/avg_eq_paired/norm_neg1pos1_fold'", 2, 3)
 ]
 
 # Function to run a command on two GPUs
