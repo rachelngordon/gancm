7794359a4cb0b265c791b09c7aba3f7693f3838e
Rachel Gordon <rgordon@luc.edu>
2023-10-30 15:36:15 -0500

    test learned_skip

--- gancm/gancm_mask_ct.py
+++ gancm/gancm_mask_ct.py
@@ -2,7 +2,7 @@
 import tensorflow.keras as kr
 import tensorflow as tf
 import numpy as np
-from . import modules_mask as modules
+from . import modules_mask_ct as modules
 import loss
 import evaluate
 from datetime import datetime
