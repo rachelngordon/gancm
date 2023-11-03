1f6dd5eeb0798d6473c2a69cfaeadaf03a4f1b92
Rachel Gordon <rgordon@luc.edu>
2023-11-03 18:30:21 -0500

    remove tfa

--- train_cycle.py
+++ train_cycle.py
@@ -28,7 +28,7 @@ def main(flags):
   sample_latent_vector = tf.keras.layers.Input(shape=(flags.latent_dim,))
 
   # Call the build method with the sample input shapes
-  model.build(input_shape=sample_input_ct.shape)
+  model.build(input_shape=[sample_input_ct.shape, sample_input_mri.shape])
 
 
   print(model.summary())
