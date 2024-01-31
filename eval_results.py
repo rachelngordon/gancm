1990dbbd1fe8e7c3183429022e562fe593ee36bd
Rachel Gordon <rgordon@luc.edu>
2024-01-31 16:22:19 -0600

    debug

--- eval_results.py
+++ eval_results.py
@@ -16,7 +16,8 @@ def main(flags):
   # get model path
   #path = "/grand/EVITA/ct-mri/exp_results/models/" + flags.name
   #path = "/grand/EVITA/ct-mri/uvit_results/uvit_models/" + flags.name
-  path = "~/uvit_models/" + flags.name
+  #path = "~/uvit_models/" + flags.name
+  path = "/media/aisec-102/DATA3/rachel/experiments/models/uvit_models/" + flags.name
 
   model = kr.models.load_model(path)
   evaluate.uvit_evaluate(flags, model, test_data)
