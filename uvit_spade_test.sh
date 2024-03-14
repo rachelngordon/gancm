c0f8c6201076dea72ce8bdfdd7ccaa4cf4e50abf
Rachel Gordon <rgordon@luc.edu>
2024-03-14 14:07:00 -0500

    debug

--- uvit_spade_test.sh
+++ uvit_spade_test.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 module load conda
 conda activate 
-python3 ~/gancm/train_uvit_spade.py --exp_name uvit_spade_test --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold' --epochs 10 --epoch_interval 1 --batch_size 1
+python3 ~/gancm/train_uvit_spade.py --exp_name uvit_spade_test --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold' --epochs 100 --epoch_interval 5 --batch_size 1
