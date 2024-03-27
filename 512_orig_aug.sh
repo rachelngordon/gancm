c87ab198e2fdefb11047e638f0526959df33cb3e
Rachel Gordon <rgordon@luc.edu>
2024-03-27 15:39:20 -0500

    update

--- 512_orig_aug.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash
-module load conda
-conda activate 
-python3 ~/gancm/train_gancm_512_aug.py --exp_name gancm_512_orig_aug --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold' --epochs 20 --epoch_interval 5 --batch_size 32
