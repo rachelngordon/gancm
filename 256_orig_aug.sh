a86a584c8bc1d8a2e1f6f03c135d65168e4445ee
Rachel Gordon <rgordon@luc.edu>
2024-04-02 16:44:49 -0500

    predict

--- 256_orig_aug.sh
+++ 256_orig_aug.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 module load conda
 conda activate 
-python3 ~/gancm/train_gancm_aug_mask.py --exp_name gancm_256_aug_no_resize_crop --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold' --epochs 20 --epoch_interval 5 --batch_size 32
+python3 ~/gancm/train_gancm_aug_mask.py --exp_name gancm_256_aug_no_central_crop --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold' --epochs 20 --epoch_interval 5 --batch_size 32
