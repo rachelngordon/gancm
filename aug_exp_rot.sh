faa10d60e1509adeb63e5dd760927ec3a4603be2
Rachel Gordon <rgordon@luc.edu>
2024-04-03 11:37:40 -0500

    exp

---
+++ aug_exp_rot.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+module load conda
+conda activate 
+python3 ~/gancm/train_gancm_aug_mask.py --exp_name gancm_256_aug_no_crop_rot45 --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_mask/norm_mask_neg1pos1_fold' --epochs 20 --epoch_interval 5 --batch_size 32 --crop_val 1 --rot_angle 45
