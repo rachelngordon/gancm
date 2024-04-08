305993189c555c6e0ba1aa141c5568cfe519eeed
Rachel Gordon <rgordon@luc.edu>
2024-04-08 16:07:33 -0500

    aug 4000

---
+++ aug_exp_rot_4000.py
@@ -0,0 +1,28 @@
+import subprocess
+import multiprocessing
+
+# Define the command lines and GPU IDs
+command_lines = [
+    ("python3 ~/gancm/train_gancm_aug_mask.py --exp_name gancm_256_aug_no_crop_rot45_4000 --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_seg/norm_mask_neg1pos1_fold' --epochs 4000 --test_fold 5 --epoch_interval 100 --batch_size 32 --crop_val 1 --rot_angle 45", 0, 1),
+    ("python3 ~/gancm/train_gancm_aug_mask.py --exp_name gancm_256_aug_no_crop_rot45_2000 --data_path '/eagle/EVITA/ct-mri/data/CV/avg_eq_seg/norm_mask_neg1pos1_fold' --epochs 2000 --test_fold 5 --epoch_interval 100 --batch_size 32 --crop_val 1 --rot_angle 45", 2, 3)
+]
+
+# Function to run a command on two GPUs
+def run_command(command, gpu_id1, gpu_id2):
+    gpu_option1 = f"--gpu_ids {gpu_id1}"
+    gpu_option2 = f"--gpu_ids {gpu_id2}"
+    full_command1 = command + " " + gpu_option1
+    full_command2 = command + " " + gpu_option2
+    subprocess.run(full_command1, shell=True)
+    subprocess.run(full_command2, shell=True)
+
+# Run the commands in parallel
+processes = []
+for cmd, gpu_id1, gpu_id2 in command_lines:
+    process = multiprocessing.Process(target=run_command, args=(cmd, gpu_id1, gpu_id2))
+    processes.append(process)
+    process.start()
+
+# Wait for all processes to finish
+for process in processes:
+    process.join()
---
+++ aug_exp_rot_4000.sh
@@ -0,0 +1,4 @@
+# !/bin/bash
+module load conda
+conda activate 
+python3 ~/gancm/aug_exp_rot_4000.py
